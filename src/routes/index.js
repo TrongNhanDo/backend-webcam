094ce564910245b7470938e25ea96df867373835
dtnhan <dotrongnhan15102000@gmail.com>
2024-03-26 10:22:03 +0700

    fix: apply write log by morgan

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import homeRoute from "./homeRoute.js";
 import userRoute from "./userRoute.js";
 
 const mainRoute = (app) => {
+  app.use(morgan("combined"));
   app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
